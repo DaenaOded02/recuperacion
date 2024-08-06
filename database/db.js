00dd9202c78ac4efc330f5a363221544468d7981
unknown <nathalysantander24@gmail.com>
2024-08-06 16:27:29 -0500

    Final

--- database/db.js
+++ database/db.js
@@ -1,11 +1,10 @@
 const mysql = require('mysql');
 
-
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root', 
     password: '',
-    database: 'login_db' 
+    database: 'recuperacion' 
 });
 
 connection.connect((err) => {
